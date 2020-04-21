75d3308103d6f96c48a0a1a9784db85ddc9af6b2
wy-hash <wy-hash@github.com>
2020-04-21 18:48:48 +0900

    create front-end scaffold
    프론트엔드 뼈대 구성
    unit 테스트
    e2e 테스트

---
+++ front-end/src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import LoginPage from '@/views/LoginPage.vue'
+
+Vue.use(VueRouter)
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [{
+    path: '/login',
+    name: 'LoginPage',
+    component: LoginPage
+  }]
+})
+
+export default router
